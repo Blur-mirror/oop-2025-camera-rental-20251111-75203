10e0271ec93473aa4ab0f06a79e822b865b84503
Antonio <antonioc.adolfo@gmail.com>
2025-11-11 15:59:58 +0000

    minor tweaks

--- obj/Debug/net8.0/assessment.AssemblyInfo.cs
+++ obj/Debug/net8.0/assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6217038d2474f542f8830edd55a64cfd224d335f")]
 [assembly: System.Reflection.AssemblyProductAttribute("assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
